6b4bf9cb8b3a98719c14dc6f6b8f370bf8f3286f
ezio84 <brabus84@gmail.com>
2017-09-30 09:22:18 +0200

    Colorpicker and customseekbar: add method to set default values
    also be sure default values is not greater than max value
    
    Change-Id: If0604bf47fd66d419581379807b3a8b05c90c886

--- src/net/margaritov/preference/colorpicker/ColorPickerPreference.java
+++ src/net/margaritov/preference/colorpicker/ColorPickerPreference.java
@@ -281,6 +281,10 @@ public class ColorPickerPreference extends Preference implements
         onColorChanged(color);
     }
 
+    public void setDefaultColor(int color) {
+        mDefValue = color;
+    }
+
     /**
      * For custom purposes. Not used by ColorPickerPreferrence
      *
