d63b68d1093c34234ed4c97d57fdf36a8eef9b86
ezio84 <brabus84@gmail.com>
2017-10-04 21:12:06 +0200

    Add NavBar toggle [2/2]
    Change-Id: I752b64dc6a2bdf80ad0f305c0033bc99198e7ac2

--- src/org/candy/candyshop/tabs/Navigation.java
+++ src/org/candy/candyshop/tabs/Navigation.java
@@ -20,12 +20,14 @@ import android.content.Context;
 import android.content.ContentResolver;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.provider.Settings;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.PreferenceFragment;
+import android.support.v14.preference.SwitchPreference;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -34,13 +36,27 @@ import com.android.settings.Utils;
 
 public class Navigation extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
+
     private static final String MISC_CATEGORY = "navigation_category";
     private static final String TAG = "Navigation";
+
+    private SwitchPreference mNavbarToggle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.navigation);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mNavbarToggle = (SwitchPreference) findPreference("navigation_bar_enabled");
+        boolean enabled = Settings.Secure.getIntForUser(
+                resolver, Settings.Secure.NAVIGATION_BAR_ENABLED,
+                getActivity().getResources().getBoolean(
+                com.android.internal.R.bool.config_showNavigationBar) ? 1 : 0,
+                UserHandle.USER_CURRENT) == 1;
+        mNavbarToggle.setChecked(enabled);
+        mNavbarToggle.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -53,9 +69,16 @@ public class Navigation extends SettingsPreferenceFragment implements
         super.onPause();
     }
 
-
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-        final String key = preference.getKey();
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mNavbarToggle) {
+            boolean value = (Boolean) newValue;
+            Settings.Secure.putIntForUser(getActivity().getContentResolver(),
+                    Settings.Secure.NAVIGATION_BAR_ENABLED, value ? 1 : 0,
+                    UserHandle.USER_CURRENT);
+            mNavbarToggle.setChecked(value);
+            return true;
+        }
         return false;
     }
 
