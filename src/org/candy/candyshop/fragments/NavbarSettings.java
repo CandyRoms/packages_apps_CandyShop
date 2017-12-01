010594aea26e6c44d12c4a2c7447ef2c7d67fd3d
Dan Cartier <nospamdan1@gmail.com>
2017-12-01 09:49:45 -0500

    CandyShop: Update navigation menu for DUI
    Change-Id: Ie637255f7b501852e0ce9f037e32223714ba2771

--- src/org/candy/candyshop/fragments/NavbarSettings.java
+++ src/org/candy/candyshop/fragments/NavbarSettings.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.candy.candyshop;
+package org.candy.candyshop.fragments;
 
 import java.util.ArrayList;
 
