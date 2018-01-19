c15bca98c8526a4c49184a7b6eb861cdfb177740
ezio84 <brabus84@gmail.com>
2018-01-19 17:15:39 +0100

    Ambient music notifications: allow to show track info only [2/2]
    Change-Id: Ia6954344ca6607a6bc92595ebd38b815e1e47c01

--- src/org/candy/candyshop/fragments/Button.java
+++ src/org/candy/candyshop/fragments/Button.java
@@ -46,11 +46,21 @@ import java.util.ArrayList;
 public class Button extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener, Indexable {
 
+    private static final String FORCE_AMBIENT_FOR_MEDIA = "force_ambient_for_media";
+    private ListPreference mAmbientTicker;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.button);
+
+        mAmbientTicker = (ListPreference) findPreference(FORCE_AMBIENT_FOR_MEDIA);
+        int mode = Settings.System.getIntForUser(resolver,
+                Settings.System.FORCE_AMBIENT_FOR_MEDIA, 0, UserHandle.USER_CURRENT);
+        mAmbientTicker.setValue(Integer.toString(mode));
+        mAmbientTicker.setSummary(mAmbientTicker.getEntry());
+        mAmbientTicker.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -63,7 +73,16 @@ public class Button extends SettingsPreferenceFragment implements
         super.onResume();
     }
 
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mAmbientTicker) {
+            int mode = Integer.valueOf((String) newValue);
+            int index = mAmbientTicker.findIndexOfValue((String) newValue);
+            mAmbientTicker.setSummary(
+                    mAmbientTicker.getEntries()[index]);
+            Settings.System.putIntForUser(resolver, Settings.System.FORCE_AMBIENT_FOR_MEDIA,
+                    mode, UserHandle.USER_CURRENT);
+            return true;
+        ]
         return false;
     }
 
