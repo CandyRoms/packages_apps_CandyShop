b4fa99007fa434b5c77a909a5e03b5591c68b992
Kevin Princiotta <kevinprinciotta@gmail.com>
2018-02-19 14:47:59 -0500

    BatteryMeterDrawable: add battery styles [2/2] Picked from Elginsk8tr
    Change-Id: Ibe3c5ac53b20d36e13783d697edea8823aeb94e4

--- src/org/candy/candyshop/tabs/StatusBar.java
+++ src/org/candy/candyshop/tabs/StatusBar.java
@@ -37,14 +37,15 @@ import com.android.settings.Utils;
 public class StatusBar extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "StatusBar";
+    private static final String BATTERY_STYLE = "battery_style";
 
     private ListPreference mTickerMode;
     private ListPreference mNoisyNotification;
+    private ListPreference mBatteryIconStyle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         addPreferencesFromResource(R.xml.statusbar);
 
         ContentResolver resolver = getActivity().getContentResolver();
@@ -65,6 +66,10 @@ public class StatusBar extends SettingsPreferenceFragment implements
         mNoisyNotification.setValue(String.valueOf(mode));
         mNoisyNotification.setSummary(mNoisyNotification.getEntry());
 
+        mBatteryIconStyle = (ListPreference) findPreference(BATTERY_STYLE);
+        mBatteryIconStyle.setValue(Integer.toString(Settings.Secure.getInt(resolver,
+               Settings.Secure.STATUS_BAR_BATTERY_STYLE, 0)));
+        mBatteryIconStyle.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -95,6 +100,11 @@ public class StatusBar extends SettingsPreferenceFragment implements
             int index = mNoisyNotification.findIndexOfValue((String) newValue);
             mNoisyNotification.setSummary(
                     mNoisyNotification.getEntries()[index]);
+            return true;
+        } else  if (preference == mBatteryIconStyle) {
+            int value = Integer.valueOf((String) newValue);
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.STATUS_BAR_BATTERY_STYLE, value);
             return true;
         }
 
