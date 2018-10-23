09969a116812aefc37d2a629a39aaf47dd63bcd0
Dan Cartier <nospamdan1@gmail.com>
2018-10-23 14:42:00 -0400

    GestureSettings: Add missing imports for navbar gestures
    Change-Id: I2f89424adb4106dd3cdee9b2cce5f057bec739c9

--- src/org/candy/candyshop/fragments/GestureSettings.java
+++ src/org/candy/candyshop/fragments/GestureSettings.java
@@ -17,21 +17,24 @@
 
 package org.candy.candyshop.fragments;
 
-import com.android.settings.SettingsPreferenceFragment;
-
+import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceScreen;
 import android.provider.Settings;
 import android.provider.SearchIndexableResource;
-
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
+import com.android.settings.SettingsPreferenceFragment;
 
 import java.util.List;
 import java.util.Arrays;
