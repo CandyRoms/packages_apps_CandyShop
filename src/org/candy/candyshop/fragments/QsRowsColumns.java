1268de6193dbf15d2ac62be2d4861d4e72731711
Dan Cartier <nospamdan1@gmail.com>
2018-11-02 10:57:51 -0400

    search: Add QsRowsColumns to index
    Change-Id: I0ad033c3b2de9eb5842e96a3597f9364d3761a6d

--- src/org/candy/candyshop/fragments/QsRowsColumns.java
+++ src/org/candy/candyshop/fragments/QsRowsColumns.java
@@ -20,13 +20,22 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.support.v7.preference.Preference;
+import android.provider.SearchIndexableResource;
 import android.provider.Settings;
 
 import org.candy.candyshop.preference.CustomSeekBarPreference;
 import com.android.internal.logging.nano.MetricsProto;
 
 import com.android.settings.R;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
 import com.android.settings.SettingsPreferenceFragment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 
 public class QsRowsColumns extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
@@ -97,4 +106,26 @@ public class QsRowsColumns extends SettingsPreferenceFragment implements
         }
         return false;
     }
+
+    public static final Indexable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    ArrayList<SearchIndexableResource> result =
+                            new ArrayList<SearchIndexableResource>();
+
+                    SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.custom_qs_rows_columns;
+                    result.add(sir);
+
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    ArrayList<String> result = new ArrayList<String>();
+                    return result;
+                }
+            };
 }
