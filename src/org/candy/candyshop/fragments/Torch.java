76f750832603f250fc7272755a9f3513370f66dd
Dan Cartier <nospamdan1@gmail.com>
2018-11-02 10:59:41 -0400

    search: Add Torch options to search index
    Change-Id: I2c5fdd219875bfe68b7c929f022608485db67798

--- src/org/candy/candyshop/fragments/Torch.java
+++ src/org/candy/candyshop/fragments/Torch.java
@@ -27,13 +27,22 @@ import android.support.v7.preference.ListPreference;
 import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.provider.SearchIndexableResource;
 import android.provider.Settings;
 import android.widget.Toast;
 
 import com.android.settings.R;
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.internal.util.candy.CandyUtils;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
 import com.android.settings.SettingsPreferenceFragment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import com.android.settings.Utils;
 
 public class Torch extends SettingsPreferenceFragment implements
@@ -100,4 +109,26 @@ public class Torch extends SettingsPreferenceFragment implements
         }
          return false;
     }
+
+    public static final Indexable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    ArrayList<SearchIndexableResource> result =
+                            new ArrayList<SearchIndexableResource>();
+
+                    SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.torch;
+                    result.add(sir);
+
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    ArrayList<String> result = new ArrayList<String>();
+                    return result;
+                }
+            };
 }
