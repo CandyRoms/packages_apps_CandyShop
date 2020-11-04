BlissRoms/platform_packages_apps_CustomDoze

Performances: move settings check into the ExecutorService
loading from settings provider on each screen on/off event
can be a slow, and we need max speed when unlocking the phone.

src/com/custom/ambient/display/ProximitySensor.java
@@ -94,15 +94,22 @@ public class ProximitySensor implements SensorEventListener {
     protected void enable() {
         if (DEBUG) Log.d(TAG, "Enabling");
         submit(() -> {
-            mSensorManager.registerListener(this, mSensor,
-                    SensorManager.SENSOR_DELAY_NORMAL);
+            if (Utils.handwaveGestureEnabled(mContext) ||
+                    Utils.pocketGestureEnabled(mContext)) {
+                mSensorManager.registerListener(this, mSensor,
+                        SensorManager.SENSOR_DELAY_NORMAL);
+            }
         });
     }
 
     protected void disable() {
         if (DEBUG) Log.d(TAG, "Disabling");
         submit(() -> {
-            mSensorManager.unregisterListener(this, mSensor);
+            if (Utils.handwaveGestureEnabled(mContext) ||
+                    Utils.pocketGestureEnabled(mContext)) {
+                mSensorManager.unregisterListener(this, mSensor);
+            }
+
         });
     }
 }

CandyRoms/packages_apps_CandyShop

Change-Id: I278f080d9bd48c000e4798b2361cf7708d568172

src/org/candy/candyshop/fragments/doze/ProximitySensor.java
@@ -108,15 +108,21 @@ public class ProximitySensor implements SensorEventListener {
     protected void enable() {
         if (DEBUG) Log.d(TAG, "Enabling");
         submit(() -> {
-            mSensorManager.registerListener(this, mSensorProximity,
-                    SensorManager.SENSOR_DELAY_NORMAL);
+            if (Utils.handwaveGestureEnabled(mContext) ||
+                    Utils.pocketGestureEnabled(mContext)) {
+                mSensorManager.registerListener(this, mSensorProximity,
+                        SensorManager.SENSOR_DELAY_NORMAL);
+            }
         });
     }
 
     protected void disable() {
         if (DEBUG) Log.d(TAG, "Disabling");
         submit(() -> {
-            mSensorManager.unregisterListener(this, mSensorProximity);
+            if (Utils.handwaveGestureEnabled(mContext) ||
+                    Utils.pocketGestureEnabled(mContext)) {
+                mSensorManager.unregisterListener(this, mSensorProximity);
+            }
         });
     }
 }